ae685b1e5e97364d24b6568914b28e4da7f6eeff
Rohit <raheet@hotmail.co.uk>
2022-11-15 16:34:13 +0000

    added some error handling to user inputs, events that focus into the input

--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -19,7 +19,13 @@ export default function Terminal() {
   //tracking for mouse and whether currently dragging
   const [dragging, setDragging] = useState(false);
 
+  const terminal = document.querySelector(".terminal");
   const topbar = document.querySelector(".terminal__top-bar");
+
+  //set focus on the input when clicking terminal
+  const focusInput = () => {
+    document.getElementById("terminal__input")!.focus()
+  }
 
   useEffect(() => {
     //changes state of dragging from false to true
@@ -36,6 +42,14 @@ export default function Terminal() {
       setDragging(false);
     }
 
+    //checks to see if terminal was clicked
+    const _handleClick = (ev :any) => {
+      if (ev.target === terminal && ev.target !== topbar) {
+        focusInput()
+      }
+    }
+
+    window.addEventListener("click", _handleClick);
     //add event listeners for events
     if (dragging)
     window.addEventListener("mouseup", _handleMouseup);
@@ -43,9 +57,10 @@ export default function Terminal() {
 
     //useEffect return
     return () => {
-        if (dragging)
-        window.removeEventListener("mousedown", _handleMousedown);
-        else window.removeEventListener("mouseup", _handleMouseup);
+      window.removeEventListener("click", _handleClick);
+      if (dragging)
+      window.removeEventListener("mousedown", _handleMousedown);
+      else window.removeEventListener("mouseup", _handleMouseup);
     }
 
   })
@@ -87,6 +102,8 @@ export default function Terminal() {
   };
 
   function _handleKeyDown(e: any) {
+      focusInput();
+    const view = document.querySelector(".terminal__view");
     if (e.key === 'Enter') {
       if (input === 'clear') {
         setTerminalDisplay([]);
@@ -95,11 +112,45 @@ export default function Terminal() {
         return;
       }
 
-      if (input === 'get quote') {
-        getQuote();
+      if (input) {
+        //if "get"
+        if (input.slice(0,3) === 'get') {
+          //if "get quote"
+          if (input === 'get quote') {
+            getQuote();
+          //get error: unrecognised
+          } else {
+            setTerminalDisplay((previousState) => [
+              ...previousState,
+              "command not recognised. try \"get quote\""
+            ]);
+          }
+        //if "fetch"
+        } else if (input.slice(0,5) === "fetch") {
+          //if "fetch weather"
+          if (input === "fetch weather") {
+            setTerminalDisplay((previousState) => [
+              ...previousState,
+              "weather called"
+            ]);
+            setInput('');
+          //fetch error: unrecognised
+          } else {
+            setTerminalDisplay((previousState) => [
+              ...previousState,
+              "command not recognised. try \"fetch weather\""
+            ]);
+          }
+        } else {
+          setTerminalDisplay((previousState) => [
+            ...previousState,
+            "command not recognised. try \"get\" or \"fetch\""
+          ]);
+        }
+      } else {
+        setTerminalDisplay((previousState) => [...previousState, input]);
       }
-
-      setTerminalDisplay((previousState) => [...previousState, input]);
+      if (view) { view.scrollTop = view.scrollHeight }
       setInput('');
     }
   }
@@ -117,7 +168,8 @@ export default function Terminal() {
         ))}
         <div className="terminal__bottom">
           <span>root@tussle:~$ </span>
-          <input
+          <input autoFocus
+            id="terminal__input"
             className="terminal__input"
             type="text"
             onKeyDown={_handleKeyDown}
